949a329ed4b091f28f94fb69e8bcaabb77b0362c
Nikolas Thitu <nikolas@hauz.io>
2015-12-09 18:20:40 -0500

    cleanup / refactoring

--- lib/stamps/response.rb
+++ lib/stamps/response.rb
@@ -8,15 +8,15 @@ module Stamps
 
     # Expects an <tt>Savon::SOAP::Response</tt> and handles errors.
     def initialize(response)
-      self.errors = []
-      self.valid = true
-      self.savon  = response
-      self.http   = response.http
-      self.hash   = self.savon.to_hash
+      self.errors   = []
+      self.valid    = true
+      self.response = response
+      self.http     = response.http
+      self.hash     = self.response.to_hash
       raise_errors
     end
 
-    attr_accessor :savon, :http, :errors, :valid, :hash, :code
+    attr_accessor :response, :http, :errors, :valid, :hash, :code
 
     # Returns the SOAP response body as a Hash.
     def to_hash
@@ -37,36 +37,36 @@ module Stamps
     # so that clients can process the error messages as they wish
     #
     def raise_errors
-      message =  'FIXME:  Need to parse http for response message'
-      return self.format_soap_faults if savon.soap_fault?
+      message = 'FIXME:  Need to parse http for response message'
+      return self.format_soap_faults if response.soap_fault?
 
       case http.code.to_i
-      when 200
-        return
-      when 400
-        raise BadRequest, "(#{http.code}): #{message}"
-      when 401
-        raise Unauthorized, "(#{http.code}): #{message}"
-      when 403
-        raise Forbidden, "(#{http.code}): #{message}"
-      when 404
-        raise NotFound, "(#{http.code}): #{message}"
-      when 406
-        raise NotAcceptable, "(#{http.code}): #{message}"
-      when 500
-        raise InternalServerError, "Stamps.com had an internal error. (#{http.code}): #{message}"
-      when 502..503
-        raise ServiceUnavailable, "(#{http.code}): #{message}"
+
+        when 200
+          return
+        when 400
+          raise BadRequest, "(#{http.code}): #{message}"
+        when 401
+          raise Unauthorized, "(#{http.code}): #{message}"
+        when 403
+          raise Forbidden, "(#{http.code}): #{message}"
+        when 404
+          raise NotFound, "(#{http.code}): #{message}"
+        when 406
+          raise NotAcceptable, "(#{http.code}): #{message}"
+        when 500
+          raise InternalServerError, "Stamps.com had an internal error. (#{http.code}): #{message}"
+        else
+          raise ServiceUnavailable, "(#{http.code}): #{message}"
       end
     end
 
     # Include any errors in the response
     #
     def format_soap_faults
-      fault = self.hash.delete("soap:Fault") || self.hash.delete(:fault)
-      self.errors << (fault[:faultstring] || fault["faultstring"])
+      fault = self.hash.delete('soap:Fault') || self.hash.delete(:fault)
+      self.errors << (fault[:faultstring] || fault['faultstring'])
       self.valid = false
     end
-
   end
 end
